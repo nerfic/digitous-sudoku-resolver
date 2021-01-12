6aea26007632536c95db23729bbcb51508bf8a04
Nerfic <jdll.allan@gmail.com>
2021-01-12 14:55:04 +0100

    fix isBlockValid function

--- generate.js
+++ generate.js
@@ -29,15 +29,8 @@ $(document).ready(function () {
         $(sudokuGame).html(soduku.join('\<br>'))
     }
 
-    displaySudoku()
-
-    $(newSudoku).click(function () {
-        generateSudoku()
-        displaySudoku()
-    })
 
     function isRowValid(line, number) {
-
         for (var i = 0; i < 9; i++) {
             if (soduku[line][i] === number) {
                 return false;
@@ -56,10 +49,33 @@ $(document).ready(function () {
     }
 
     function isBlockValid(line, column, number) {
-        for (var i = 0; i < 3; i++) {
-            for (var j = 0; j < 3; j++) {
-                console.log(soduku[line + i][column + j])
-                if (soduku[line + i][column + j] === number) {
+        var startI = 0;
+        var startJ = 0;
+
+        if (line === 0 || line === 1 || line === 2) {
+            startI = 0
+        } else if (line === 3 || line === 4 || line === 5) {
+            startI = 3
+        } else if (line === 6 || line === 7 || line === 8) {
+            startI = 6
+        } else {
+            console.log("Error")
+        }
+
+        if (column === 0 || column === 1 || column === 2) {
+            startJ = 0
+        } else if (column === 3 || column === 4 || column === 5) {
+            startJ = 3
+        } else if (column === 6 || column === 7 || column === 8) {
+            startJ = 6
+        } else {
+            console.log("Error")
+        }
+
+        for (var i = startI; i < startI + 3; i++) {
+            for (var j = startJ; j < startJ + 3; j++) {
+                console.log(soduku[i][j])
+                if (soduku[i][j] === number) {
                     return false;
                 }
             }
@@ -67,7 +83,30 @@ $(document).ready(function () {
         return true;
     }
 
-    // console.log(isRowValid(8, 9))
-    // console.log(isColumnValid(0, 8))
-    console.log(isBlockValid(0, 0, 7))
+    function isValid() {
+
+        for (var i = 0; i < 9; i++) {
+            for (var j = 0; j < 9; j++) {
+                var randomNumber = Math.floor(Math.random() * 9 + 1)
+                if (sudoku[i][j] === 0) {
+                    if (isRowValid(j, randomNumber) === true) {
+
+                    } else {
+
+                    }
+                } else {
+
+                }
+            }
+        }
+    }
+
+    displaySudoku()
+
+    $(newSudoku).click(function () {
+        generateSudoku()
+        displaySudoku()
+    })
+
+    console.log(isBlockValid(0, 3, 8))
 })
